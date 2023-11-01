4d6df44041bfab80e17e3e51a97a85c024e3f03f
Kobold831 <k1z3y1.y15n5d1.1022@gmail.com>
2023-11-01 21:01:59 +0900

    Fixes

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    compileSdk = 33
+    namespace = "com.saradabar.cpadcustomizetool"
+
+    defaultConfig {
+        minSdk = 22
+        //noinspection ExpiredTargetSdkVersion
+        targetSdk = 22
+
+        versionCode = 39
+        versionName = "1.6.0"
+
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+
+        debug {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        aidl = true
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to arrayOf("*.jar"))))
+    implementation("androidx.preference:preference:1.2.1")
+    implementation("com.stephentuso:welcome:1.4.1")
+    implementation("org.zeroturnaround:zt-zip:1.12")
+}
