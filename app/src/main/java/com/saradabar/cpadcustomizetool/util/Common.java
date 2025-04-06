dc6b2ccbf4185dce1c1a39b6ce814075ddd51cf9
Syuugo <pub@s1204.me>
2025-04-06 22:05:27 +0900

    unused

--- app/src/main/java/com/saradabar/cpadcustomizetool/util/Common.java
+++ app/src/main/java/com/saradabar/cpadcustomizetool/util/Common.java
@@ -19,7 +19,6 @@ import android.net.Uri;
 import android.os.BenesseExtension;
 import android.os.Build;
 import android.os.Environment;
-import android.os.ServiceManager;
 import android.provider.DocumentsContract;
 import android.provider.OpenableColumns;
 import android.util.Log;
